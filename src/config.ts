2f34da725f94c1e9439ab8fb622e857738c1d50e
Gasatrya <gasatrya.dev@gmail.com>
2022-09-08 12:05:17 +0700

    Update

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const SITE = {
+  title: 'Hidup Berdua',
+  description: 'rumah tangga, sebuah proses.',
+  url: 'https://hidupberdua.id',
+  locale: 'id',
+  author: {
+    name: 'Ga Satrya',
+  },
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+const em = (px, base) => `${round(px / base)}em`
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Source Serif Pro"', ...defaultTheme.fontFamily.serif],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      typography: {
+        xl: {
+          css: [
+            {
+              lineHeight: 1.5,
+              h1: {
+                marginBottom: 0,
+              },
+            },
+          ],
+        },
+        DEFAULT: {
+          css: {
+            a: {
+              textDecoration: false,
+              fontWeight: '400',
+            },
+            'h2 a': {
+              fontWeight: '800',
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
